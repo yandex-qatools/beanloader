2ff5d7e807c9f45e10252d865608800af764143e
innokenty <innokenty@yandex-team.ru>
2015-09-07 18:38:06 +0300

    improve FileWatcherLoadStrategyTest

--- src/test/java/ru/qatools/beanloader/FileWatcherLoadStrategyTest.java
+++ src/test/java/ru/qatools/beanloader/FileWatcherLoadStrategyTest.java
@@ -1,56 +1,73 @@
 package ru.qatools.beanloader;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 import static ru.qatools.beanloader.BeanAssert.BEAN_XML_NAME;
 import static ru.qatools.beanloader.BeanAssert.RESOURCES_DIR;
 import static ru.qatools.beanloader.BeanAssert.getActualValue;
 import static ru.qatools.beanloader.BeanAssert.setActualValue;
+import static ru.qatools.beanloader.BeanLoader.load;
 import static ru.qatools.beanloader.BeanLoaderStrategies.fileWithWatcher;
+import static ru.qatools.beanloader.TestBeanChangeListener.beCalledWith;
+import static ru.yandex.qatools.matchers.decorators.MatcherDecorators.should;
+import static ru.yandex.qatools.matchers.decorators.MatcherDecorators.timeoutHasExpired;
 
 /**
  * @author Innokenty Shuvalov innokenty@yandex-team.ru
  */
 public class FileWatcherLoadStrategyTest extends BeanChangingTest {
 
-    @Test
-    public void testFileWatcher() throws Exception {
-        TestBeanChangeListener listener = new TestBeanChangeListener();
-        BeanAssert assertBean = new BeanAssert(fileWithWatcher(RESOURCES_DIR, BEAN_XML_NAME, listener));
+    private TestBeanChangeListener listener;
 
-        assertTrue(listener.isOnChangeMethodCalled());
-        listener.reset();
+    private BeanLoader<Bean> beanLoader;
 
-        assertBean.valueIsEqualToActual();
-
-        Thread.sleep(1000);
-        String newValue = "another " + getActualValue();
-        setActualValue(newValue);
-        assertBean.valueHasNotChanged();
-
-        assertFalse(listener.isOnChangeMethodCalled());
-
-        assertBean.waitUntilValueIsEqualTo(newValue);
-        assertTrue(listener.isOnChangeMethodCalled());
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        listener = new TestBeanChangeListener();
+        beanLoader = load(Bean.class).from(fileWithWatcher(RESOURCES_DIR, BEAN_XML_NAME, listener));
     }
 
-    private class TestBeanChangeListener implements BeanChangeListener<Bean> {
+    @Test
+    public void testListenerIsCalledImmediately() throws Exception {
+        assertTrue(listener.isCalledWith(getActualValue()));
+    }
 
-        boolean onChangeMethodCalled;
+    @Test
+    public void testListenerIsNotCalledOnBeanGetter() {
+        listener.reset();
+        beanLoader.getBean();
+        assertTrue(listener.isNotCalled());
+    }
 
-        public boolean isOnChangeMethodCalled() {
-            return onChangeMethodCalled;
-        }
+    @Test
+    public void testListenerInvocation() throws Exception {
+        // I have no idea how this affects the test's result,
+        // but without this line it's quite unstable on my machine.
+        // Seems like we should wait after initializing WatchService,
+        // but before writing a new value to the file
+        Thread.sleep(1000);
 
-        public void reset() {
-            onChangeMethodCalled = false;
-        }
+        listener.reset();
+        String newValue = "another " + getActualValue();
+        setActualValue(newValue);
+        assertThat(listener, should(beCalledWith(newValue))
+                .whileWaitingUntil(timeoutHasExpired(60000)));
+    }
 
-        @Override
-        public void beanChanged(Bean newBean) {
-            onChangeMethodCalled = true;
-        }
+    @Test
+    public void testWatcherIsStoppedOnGarbageCollection() throws Exception {
+        Thread.sleep(1000);
+        listener.reset();
+        beanLoader = null;
+        System.gc();
+        String newValue = "another " + getActualValue();
+        setActualValue(newValue);
+        Thread.sleep(15000);
+        assertTrue(listener.isNotCalled());
     }
 }
---
+++ src/test/java/ru/qatools/beanloader/TestBeanChangeListener.java
@@ -0,0 +1,46 @@
+package ru.qatools.beanloader;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class TestBeanChangeListener implements BeanChangeListener<Bean> {
+
+    private String newValue;
+
+    public boolean isCalledWith(String value) {
+        return value.equals(newValue);
+    }
+
+    public boolean isNotCalled() {
+        return newValue == null;
+    }
+
+    public void reset() {
+        newValue = null;
+    }
+
+    @Override
+    public void beanChanged(Bean newBean) {
+        newValue = newBean.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TestBeanChangeListener{newValue='%s'}", newValue);
+    }
+
+    public static Matcher<? super TestBeanChangeListener> beCalledWith(final String value) {
+        return new TypeSafeMatcher<TestBeanChangeListener>() {
+            @Override
+            protected boolean matchesSafely(TestBeanChangeListener listener) {
+                return listener.isCalledWith(value);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("not called with value '" + value + "'");
+            }
+        };
+    }
+}
